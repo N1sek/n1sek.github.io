61cb51dfcf42f289337188c1fbd45a43cc71968a
N1sek <denisrobcizma@gmail.com>
2022-12-19 13:07:48 +0100

    Fix projects section. Show max 3

--- repos.js
+++ repos.js
@@ -3,14 +3,15 @@ const reposDiv = document.getElementById("cards");
 
 async function getRepos() {
   const response = await fetch(`https://api.github.com/users/${username}/repos`);
-  const repos = await response.json();
+  //Get only the 3 most starred repos and place the most starred in the middle between the other two
+  const repos = (await response.json()).sort((a, b) => b.stargazers_count - a.stargazers_count).slice(0, 3);
   for (let repo of repos) {
     const repoName = repo.name;
     const repoLink = repo.html_url;
     const repoDescription = repo.description;
     const repoStars = repo.stargazers_count;
     reposDiv.innerHTML +=
-      "<div class='repo col-md-auto'>" +
+      "<div class='repo'>" +
         "<a href='" + repoLink + "' target=_blank>" + repoName + " " + "<span>&#x1F517;</span>" + "</a>" +
         "<p>" + repoDescription + "</p>" +
         "<p id='p-stars'> <span class='repo-stars'>Stars: </span> " + repoStars + "</p>" +
